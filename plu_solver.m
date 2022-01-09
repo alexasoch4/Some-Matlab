ccd5cd1f36698fae92f5a797c6c5ef4c813c3974
alexasoch4 <97301613+alexasoch4@users.noreply.github.com>
2022-01-09 10:52:46 -0500

    Update plu_solver.m

--- plu_solver.m
+++ plu_solver.m
@@ -1,10 +1,4 @@
+%function needed for
 function [x]=plu_solver(A,b)
-% Usage: [x]=lu_solver(A,b)
-% Solves the linear system Ax = b by LU factorization LUx = b
-% Input:
-% A = matrix
-% b = right hand side vector
-% Output:
-% x = solution vector
-[L,U,P]=lu_fac_pivot(A) % Calculate LU factorization of a
-x = backsub(U,forsub(L,P*b)); % Solve LUx = b
+[L,U,P]=lu_fac_pivot(A) 
+x = backsub(U,forsub(L,P*b)); 
